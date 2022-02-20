a47a1ae19fd4615d1be5c1d34dd50cde66f77999
rwollensc <rodrigowollensc@gmail.com>
2022-02-20 01:10:30 +0000

    Adicioanada flake8 como dependencia
    close #12

--- README.md
+++ README.md
@@ -7,6 +7,17 @@ Link para o curso [Python Pro](https://www.python.pro.br/)
 
 Suportada versao 3 de Python
 
+Para instalar
+```console
+python3 -m venv .venv
+.venv/Scripts/activate
+pip install -r requirements-dev.txt
+```
+Para conferir qualidade de código:
+```console
+flake8
+```
+
 Tópicos a serem abordados:
 
 1. Git
--- libpythonpro/github.api.py
+++ libpythonpro/github.api.py
@@ -1,3 +1,6 @@
+import requests
+
+
 def buscar_avatar(usuario):
     """
     Busca o avatar de um usuário no Github
@@ -6,4 +9,10 @@ def buscar_avatar(usuario):
     :return: str com o link do avatar
     """
 
-    url=f'https://api.github.com/users/{usuario}'
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+
+if __name__ == '__main__':
+    print(buscar_avatar('rwollensc'))
